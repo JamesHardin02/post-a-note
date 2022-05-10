26795a842cec9e87049f83f15f9575825d4f1afb
JamesH <jwhusc7@gmail.com>
2022-05-10 10:10:09 -0400

    added route but db.json is reading undefined

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
+const { filterByQuery, findById, createNewNote, validateNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
   let results = notes;
-  // if(req.query){
-  //   results = filterByQuery(req.query, results)
-  // }
+  if(req.query){
+    results = filterByQuery(req.query, results)
+  }
   res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+  res.json(req.body);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+
+
 // Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
